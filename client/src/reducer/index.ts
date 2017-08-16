18707bde3c7a27ce2cc7458cba1c180303d69305
Ionescu Bebe <blueskymobile@hotmail.com>
2017-08-16 13:47:06 +0800

    add comment

--- client/src/reducer/index.ts
+++ client/src/reducer/index.ts
@@ -1,6 +1,6 @@
 import { combineReducers, Reducer } from 'redux';
 import { routerReducer } from 'react-router-redux';
-
+//main rootReducer
 const rootReducer: Reducer<any> = combineReducers({
     routing: routerReducer
 });
