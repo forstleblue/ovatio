fdecea24b5cd085da5a6dae89e1a1161085b6ef4
Ionescu Bebe <blueskymobile@hotmail.com>
2017-08-16 14:07:06 +0800

    master comment

--- client/src/reducer/index.ts
+++ client/src/reducer/index.ts
@@ -2,7 +2,7 @@ import { combineReducers, Reducer } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 //main rootReducer master bramch
-
+// master comment
 const rootReducer: Reducer<any> = combineReducers({
     routing: routerReducer
 });
