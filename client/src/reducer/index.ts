3f2e15fbce96dfad2e96a8684bcfbb702f2e9b53
Ionescu Bebe <blueskymobile@hotmail.com>
2017-08-16 13:48:10 +0800

    add root reducer comment

--- client/src/reducer/index.ts
+++ client/src/reducer/index.ts
@@ -4,5 +4,5 @@ import { routerReducer } from 'react-router-redux';
 const rootReducer: Reducer<any> = combineReducers({
     routing: routerReducer
 });
-
+// export root reducer
 export default rootReducer;
